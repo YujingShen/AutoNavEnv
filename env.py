7e98dae88249e96cd254cb0d31cf5fbd89683fb9
shen@MacOS <yujingshen96@gmail.com>
2017-08-13 13:53:38 +0800

    rename files

---
+++ env.py
@@ -0,0 +1,19 @@
+# -*- coding:utf8 -*-
+
+
+class AutoNavEnv(object):
+    def __init__(self):
+        pass
+
+    def make(self, file_name):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self):
+        pass
+
+    def step(self, action):
+        pass
+
